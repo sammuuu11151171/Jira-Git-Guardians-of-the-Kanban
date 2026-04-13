92b7686e061f30b48e90db7d929ead652c44886c
Nyxiaz <dishasingh3108@gmail.com>
2026-04-13 19:27:42 +0530

    added a surprise element 'Click Me ;P'

--- script1.js
+++ script1.js
@@ -1,3 +1,8 @@
 function showMessage() {
     document.getElementById("msg").innerText = "Hello Team! Guardians of the Sprint....This is Samruddhi this side :)";
 }
+
+function showImage() {
+    document.getElementById("img").innerHTML =
+    "<img src='pic.jpg' width='200'>";
+}
