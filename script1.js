22d880d9e0350c064052f6043509fab263a303fe
Kamlesh Behera <kamleshbehera420@gmail.com>
2026-04-13 22:49:59 +0530

    Added a input greeting feature

--- script1.js
+++ script1.js
@@ -1,3 +1,16 @@
 function showMessage() {
     document.getElementById("msg").innerText = "Hello Team! Guardians of the Sprint....This is Samruddhi this side :)";
 }
+
+
+// New Feature (User Input Message)
+function showCustomMessage() {
+    let name = document.getElementById("userInput").value;
+
+    if (name.trim() === "") {
+        document.getElementById("msg").innerText = "Please enter your name!";
+    } else {
+        document.getElementById("msg").innerText =
+            "Hello " + name + "! Welcome to Guardians of the Sprint 🚀";
+    }
+}
